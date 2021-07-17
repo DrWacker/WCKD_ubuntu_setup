42607fd7d031d2ff800f1331207e49ef3afb0511
DrWacker <dr.wacker@gmail.com>
2021-07-17 13:36:09 -0600

    added vim

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # WCKD_ubuntu_setup
 
 ```bash
+# Some steps might require sudo
 apt update
 apt install -y git
 git clone https://github.com/DrWacker/WCKD_ubuntu_setup.git
-
+chmod 755 WCKD_ubuntu_setup/install.sh
+WCKD_ubuntu_setup/install.sh
 ```
--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
 echo "Hello World"
+apt install -y vim
+cat <<EOT >> ~/.vimrc
+set expandtab
+set smarttab
+set shiftwidth=2
+set tabstop=2
+set ai "Auto indent
+set si "Smart indent
+
+syntax enable
+set hlsearch
+
+try
+  colorscheme slate
+catch
+endtry
+EOT
